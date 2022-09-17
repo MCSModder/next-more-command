c1009a152c89fc9578e5f561330e0e7935247d9e
深渊の鸽子 <ghzero97@gmail.com>
2022-09-17 17:35:42 +0200

    更新

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,23 +8,23 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Next更多指令扩展',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://zerxz.github.io/',
+  baseUrl: '/next-more-command/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'Zerxz', // Usually your GitHub org/user name.
+  projectName: 'next-more-command', // Usually your repo name.
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans'],
+    defaultLocale: 'zh-hans',
+    locales: ['zh-hans'],
   },
 
   presets: [
