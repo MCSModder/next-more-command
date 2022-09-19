c0930b96bf93b90464ca7e7909f1e4f78e143d4a
深渊の鸽子 <ghzero97@gmail.com>
2022-09-19 02:01:36 +0200

    更新

--- src/components/SteamCards/Workshop/index.tsx
+++ src/components/SteamCards/Workshop/index.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import ReactDom from "react-dom";
 import axios,{AxiosRequestConfig} from "axios";
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 interface ISteamCardsWorkshopProps{
   publishedfileids:number|number[]
 }
@@ -14,24 +12,41 @@ export default class SteamCardsWorkshop extends React.Component<ISteamCardsWorks
     this.publishedfileids = props.publishedfileids;
   }
  async componentDidMount(){
-  const {publishedfileids} = this.props
-   let items = Array.isArray(publishedfileids) ? publishedfileids: [publishedfileids]
-  const itemcount = items.length.toString();
-  const form = new FormData() 
-  form.append("itemcount",itemcount)
-items.forEach((el,i) => {
-  form.append(`publishedfileids[${i}]`,`${el}`)
-})
+//   const {publishedfileids} = this.props
+//    let items = Array.isArray(publishedfileids) ? publishedfileids: [publishedfileids]
+//   const itemcount = items.length.toString();
+//   const form = new FormData() 
+//   form.append("itemcount",itemcount)
+// items.forEach((el,i) => {
+//   form.append(`publishedfileids[${i}]`,`${el}`)
+// })
 
   
-console.log(FormData);
+// console.log(FormData);
 
-  const config:AxiosRequestConfig= {
-method:'post',
-data:form,
-url:'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
-  }
-   await axios(config)
+//   const config:AxiosRequestConfig= {
+// method:'post',
+// data:form,
+// url:'/dev_api/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
+// headers: {
+//   'Content-Type':'application/x-www-form-urlencoded',
+// }
+//   }
+  //await axios(config)
+
+  const result =await fetch("https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/",{
+    body:`itemcount=1&publishedfileids[0]=2844206862`,
+    method:"post",
+    headers:{
+       'Content-Type':'application/x-www-form-urlencoded',
+       "Access-Control-Allow-Origin":"*"
+    },mode:"no-cors"
+  }).then(v=>{
+    console.log(v);
+    
+  })
+  console.log(result);
+  
   }
   render(): React.ReactNode {
     return(<div>
